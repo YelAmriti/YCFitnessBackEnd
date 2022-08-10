f125cb9578496da386df119c7270e3445a5d6283
Arel <arel.bokobza@outlook.com>
2022-08-10 15:04:41 +0200

    redirection code works, user id works but creates new user

--- JuliRennen/Controller/RouteController.cs
+++ JuliRennen/Controller/RouteController.cs
@@ -8,7 +8,8 @@ using Route = JuliRennen.Models.Route;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Web;
-
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace JuliRennen.Controllers
 {
@@ -50,6 +51,7 @@ namespace JuliRennen.Controllers
             NewRoute.GPSyEnd = Convert.ToDouble(GPSyEnd);
             NewRoute.GPSxStart = Convert.ToDouble(GPSxStart);
             NewRoute.GPSxEnd = Convert.ToDouble(GPSxEnd);
+            dev = GetUser();
 
             //Save Photo
             string upload = Path.Combine("wwwroot", "images");
@@ -59,9 +61,38 @@ namespace JuliRennen.Controllers
                 Photo.CopyTo(FileStream);
             }
             NewRoute.Photo = "../images/" + Photo.FileName;
+            NewRoute.User = dev;
+
             _context.Add(NewRoute);
             _context.SaveChanges();
-            return View("WelcomeUser");
+            ViewBag.Message = _context.Route;
+            return View("SeeRoutes", ViewBag.Message);
+        }
+
+        public User GetUser()
+        {
+            _context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            var DBData = _context.User;
+            User dev = new User();
+            string userName = User.Identity.Name;
+
+            foreach (var users in DBData)
+            {
+                if (users.UserName == userName)
+                {
+                    dev.UserName = users.UserName;
+                    dev.SetPassword = users.SetPassword;
+                    dev.EmailAddress = users.EmailAddress;
+                    dev.StaminaPref = users.StaminaPref;
+                    dev.StretchPref = users.StretchPref;
+                    dev.PhoneNumber = users.PhoneNumber;
+                    dev.StaminaPref = users.StaminaPref;
+                    dev.ProfilePic = users.ProfilePic;
+                    dev.SpeedPref = users.SpeedPref;
+                    dev.StrengthPref = users.StrengthPref;
+                }
+            }
+            return dev;
         }
 
         [HttpPost]
@@ -75,18 +106,27 @@ namespace JuliRennen.Controllers
             NewRoute.GPSxEnd = Convert.ToDouble(GPSxEnd);
             NewRoute.Photo = Photo;
             ViewBag.Message = NewRoute;
-            //ViewData["Photo"] = NewRoute.Photo;
+            
             return View();
         }
 
+        [Authorize]
         public ActionResult WelcomeUser()
         {
             return View();
         }
 
+        [Authorize]
         public ActionResult SeeRoutes()
         {
             ViewBag.Message = _context.Route;
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult SeeRoutes(dynamic data)
+        {
+            ViewBag.Message = data;
             return View();
         }
 

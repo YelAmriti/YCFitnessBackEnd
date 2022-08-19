27a7692870acde8db15357ae735be9f63ba28d41
Arel <arel.bokobza@outlook.com>
2022-08-19 13:04:36 +0200

    new bugs

--- JuliRennen/Controller/RouteController.cs
+++ JuliRennen/Controller/RouteController.cs
@@ -89,9 +89,9 @@ namespace JuliRennen.Controllers
             {
                 string base64 = PhotoData.Substring(PhotoData.LastIndexOf(',') + 1);
                // Image image = MakeImage(base64);
-                string wwwPath = this.Environment.WebRootPath;
-                string upload = Path.Combine(wwwPath, "images");
-                filepath = Path.Combine(upload, fileName);
+               // string wwwPath = this.Environment.WebRootPath;
+               // string upload = Path.Combine(wwwPath, "images");
+                //filepath = Path.Combine(upload, fileName);
                 //image.Save(filepath);
             }    
             NewRoute.Photo = filepath;

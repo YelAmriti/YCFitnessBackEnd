325ca24f96879cbbafd06222edfd93ef0c63f6f8
Arel <arel.bokobza@outlook.com>
2022-08-19 15:54:14 +0200

    I think I finally fixed it..

--- JuliRennen/wwwroot/js/site.js
+++ JuliRennen/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var imageSrc = $(".card-img").find('img').attr("src");
+imageSrc = imageSrc.replace(';', '');
+$(".card-img").attr("src", imageSrc);
